1ab94af2d551a9b86f46f00ab6432d8a765945c0
Eduardo Porto <A01027893@tec.mx>
2024-04-22 14:11:56 -0600

    Added app.post

--- WebDesign/Tareas/pruebasAPI/hello_server/app.js
+++ WebDesign/Tareas/pruebasAPI/hello_server/app.js
@@ -1,11 +1,16 @@
 "use strict"
 import express from 'express';
 
+const port= 5000;
 const app= express();
+let cards= [];
+
+app.use(express.json());
 
 app.get(
-    "/name",  (req, res)=>{
+    "/hello",  (req, res)=>{
         const salute= "Hello from server";
+        console.log(req.query);
         res.status(200).send(salute);
     }
 );
@@ -15,10 +20,17 @@ app.get("/hello/:name", (req, res)=>{
         const salute=  `Hello ${req.params.name}!!`;
         res.status(200).send(salute);
     }
+    );
+    
+    app.post("/cards",  (req, res)=>{
+        console.log(req.body);
+        cards.push(req.body)
+        res.status(200).send("Card is added successfuly");
+    }
 );
 
-app.listen(3000, ()=>{
-        console.log("Running on port 3000");
+app.listen(port, ()=>{
+        console.log(`Running on port ${port}`);
     }
 );
 
